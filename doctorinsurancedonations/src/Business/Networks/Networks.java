e30f0f35469469f36826e7b3aef835e601431ffd
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-04-22 08:56:30 -0400

    Networks

---
+++ doctorinsurancedonations/src/Business/Networks/Networks.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Networks;
+
+import Business.Enterprise.EntDirectory;
+
+/**
+ *
+ * @author Pratik
+ */
+public class Networks {
+    private String networkName;
+    private EntDirectory entDir;
+    
+    public Networks(){
+        entDir=new EntDirectory();
+    }
+    public String getNetwrkName() {
+        return networkName;
+    }
+
+    public void setNetwrkName(String netwrkNm) {
+        this.networkName = netwrkNm;
+    }
+
+    public EntDirectory getEntDir() {
+        return entDir;
+    }
+    
+    @Override
+    public String toString(){
+        return networkName;
+    }
+    
+}
