5b2bbd56b675488033041215b078bb7fdd17263e
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-04-22 01:24:20 -0400

    Employee

---
+++ HealthCareAutomation/src/Business/Employee/StaffDirectory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dineshmurugesan
+ */
+public class StaffDirectory {
+
+    private ArrayList<Staff> empList;
+
+    public StaffDirectory() {
+        empList = new ArrayList();
+    }
+
+    public ArrayList<Staff> getEmpList() {
+        return empList;
+    }
+
+    public Staff createEmployee(String name) {
+        Staff employee = new Staff();
+        employee.setEmpName(name);
+        empList.add(employee);
+        return employee;
+    }
+}
