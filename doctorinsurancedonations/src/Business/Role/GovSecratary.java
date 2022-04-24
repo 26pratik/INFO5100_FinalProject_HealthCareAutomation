a36f1708ce5ed1fe3e4190205cae7616c0aa0c2f
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-04-24 11:58:39 -0400

    Roles

---
+++ doctorinsurancedonations/src/Business/Role/GovSecratary.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Org.Organization;
+import Business.Org.OrgSecretary;
+import Business.SystemUserAccount.SystemUsers;
+import javax.swing.JPanel;
+import userinterface.GovernmentSecretaryRole.SecretaryWorkAreaJPanel;
+
+/**
+ *
+ * @author Pratik
+ */
+public class GovSecratary extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, SystemUsers userAccount, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new SecretaryWorkAreaJPanel(userProcessContainer, userAccount, (OrgSecretary)organization, enterprise);
+    }
+    
+}
