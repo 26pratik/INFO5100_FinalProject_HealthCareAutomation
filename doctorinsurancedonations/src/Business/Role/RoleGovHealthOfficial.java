46d593a983eb9098fb44ba433d6125fa9668d6c4
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-04-25 12:15:55 -0400

    Roles added

---
+++ doctorinsurancedonations/src/Business/Role/RoleGovHealthOfficial.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Org.OrgHealthCareOfficial;
+import Business.Org.Organization;
+import Business.SystemUserAccount.SystemUsers;
+import javax.swing.JPanel;
+import userinterface.HealthcareOfficerRole.OfficerWorkAreaJPanel;
+
+/**
+ *
+ * @author Pratik
+ */
+public class RoleGovHealthOfficial extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, SystemUsers userAccount, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new OfficerWorkAreaJPanel(userProcessContainer, userAccount, (OrgHealthCareOfficial)organization, enterprise);
+    }
+    
+}
