3ebe132607ad66ca68407b3937c68c81db8ab79b
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-05-01 21:30:54 -0400

    Work Q code doc

--- doctorinsurancedonations/src/Business/WorkQueue/NgoDonorReq.java
+++ doctorinsurancedonations/src/Business/WorkQueue/NgoDonorReq.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Business.WorkQueue;
 
 /**
@@ -9,16 +6,16 @@ package Business.WorkQueue;
  * @author Parth
  */
 public class NgoDonorReq extends WorkReq{
-    private String nameOfDonor;
-    private String addressOfDonor;
-    private int donation;
+    private String nameOfDonor; //Name of Donor
+    private String addressOfDonor; //Address of Donor
+    private int donation; //Donation
     
     public NgoDonorReq(String nameOfDonor, String addressOfDonor, int donation){
         this.nameOfDonor = nameOfDonor;
         this.addressOfDonor = addressOfDonor;
         this.donation = donation;
     }
-    
+    //Getters and Setters
     @Override
     public String toString() {
         return nameOfDonor;
